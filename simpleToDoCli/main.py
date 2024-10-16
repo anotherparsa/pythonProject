70fedb6375f5c8b484211a2ad2e40a338fb4d65a
anotherparsa <anotherparsa@gmail.com>
2024-10-16 04:34:48 +0330

    storing users task on a file

--- simpleToDoCli/main.py
+++ simpleToDoCli/main.py
@@ -1,19 +1,24 @@
 todos = []
 user_input = ""
 
+
 while user_input != "exit":
     user_input = input("What do you want to do? 1-Add 2-Show 3-Edit 4-Delete 5-Exit ").strip()
     match user_input:
         case "1":
-            task = ""
-            while task != "done":
-                task = input("what is your task? type done to return ").strip()
-                if task != "done":
-                    todos.append(task)
+            with open("todos.txt", "a") as file:
+                task = ""
+                while task != "done":
+                    task = input("what is your task? type done to return ").strip()
+                    if task != "done":
+                        todos.append(task)
+                        file.writelines(task+'\n')
+
+
         case "2" :
             print("Showing the tasks: ")
             for i in range(0, len(todos)):
-                print(f'number {i+1} task is {todos[i]}\n')
+                print(f'number {i+1} task is {todos[i]}')
         case "3" :
             todo_number = input("enter the number of the task you want to edit: type canel to return ")
             if todo_number != "cancel":
@@ -30,4 +35,5 @@ while user_input != "exit":
                 print("Done")
         case "5" :
             print("Exiting....")
+            file.close()
             exit()
