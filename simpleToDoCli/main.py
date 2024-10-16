763474938c65803c107b62200db2e1f64eb12de0
anotherparsa <anotherparsa@gmail.com>
2024-10-16 03:34:59 +0330

    users can add and see tasks

--- simpleToDoCli/main.py
+++ simpleToDoCli/main.py
@@ -1,9 +1,17 @@
 todos = []
 user_input = ""
-while user_input != "exit":
-    user_input  = input("what is your task? type exit to exit the app: ")
-    todos.append(user_input)
-    print(user_input)
 
-print("All Todos")
-print(todos)
+while user_input != "exit":
+    user_input = input("What do you want to do? 1-Add 2-Show 3-Exit ")
+    match user_input:
+        case "1":
+            task = ""
+            while task != "done":
+                task = input("what is your task? type done to return ")
+                if task != "done":
+                    todos.append(task)
+        case "2" :
+            print(todos)
+        case "3" :
+            print("Exiting....")
+            exit()
