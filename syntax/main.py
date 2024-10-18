62fd69424793d79ded8c0e2d0075982a2744a606
anotherparsa <anotherparsa@gmail.com>
2024-10-18 22:40:56 +0330

    Number Guessing Game

--- syntax/main.py
+++ syntax/main.py
@@ -1,4 +1,22 @@
-def greet(name):
-    print(f'hello {name}')
+import random
 
-greet(input("what is your name? ").strip())
+hads = ""
+
+javab = random.randint(1, 99)
+
+chances = 5
+
+while hads != str(javab) and chances != 0:
+    print(f'answer is : {javab}')
+    hads = int(input("what is your guess? ").strip())
+    if hads == javab:
+        print("You won")
+        exit()
+    elif hads > javab:
+        print("guess smaller ")
+        chances -= 1
+    else :
+        print("guess bigger ")
+        chances -= 1
+print("You lost")
+
