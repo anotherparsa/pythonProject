9b71959a76446a6ffcde93178969266c51d4501a
anotherparsa <anotherparsa@gmail.com>
2024-10-19 04:41:16 +0330

    editing todos in the file

--- simpleToDoCli/main.py
+++ simpleToDoCli/main.py
@@ -1,46 +1,52 @@
-todos = []
 user_input = ""
 counter = 1
+
+def write_to_the_files(task):
+    with open("todos.txt", "a") as file:
+        file.writelines(str(counter) + "- " + task + '\n')
+
+def read_from_file():
+    print("Showing the task: ")
+    with open("todos.txt", "r") as file:
+        return file.readlines()
+
+def write_all_tasks_to_file(tasks):
+    with open("todos.txt", "w") as file:
+        file.writelines(tasks)
 
 while user_input != "exit":
     user_input = input("What do you want to do? 1-Add 2-Show 3-Edit 4-Delete 5-Exit ").strip()
     match user_input:
         case "1":
-            with open("todos.txt", "a") as file:
                 task = ""
                 while task != "done":
-                    task = input("what is your task? type done to return ").strip()
+                    task = input("What is your task? type done to return ").strip()
                     if task != "done":
-                        todos.append(task)
-                        file.writelines(str(counter)+"- "+ task+'\n')
+                        write_to_the_files(task)
                         counter += 1
-                    else:
-                        file.close()
 
         case "2" :
-            print("Showing the tasks: ")
-            for i in range(0, len(todos)):
-                print(f'number {i+1} task is {todos[i]}')
-            with open("todos.txt", "r") as file:
-                content = file.read()
-                print(content)
-                file.close()
-            
+            for task in read_from_file():
+                task.strip()
+                print(task)
+
         case "3" :
             todo_number = input("enter the number of the task you want to edit: type canel to return ")
             if todo_number != "cancel":
-                existing_todo = todos[int(todo_number)-1]
-                print(f'Your current number {todo_number} is {existing_todo} how do you want to change it? type cancel to return: ')
+                existing_todos = read_from_file()
+                print(f'Your current number {todo_number} is {existing_todos[int(todo_number) - 1]} how do you want to change it? type cancel to return: ')
                 updated_todo = input().strip()
                 if updated_todo != "cancel":
-                    todos[int(todo_number)-1] = updated_todo
+                    existing_todos[int(todo_number) - 1] = f"{todo_number}- " + updated_todo
+                    write_all_tasks_to_file(existing_todos)
                     print("done")
+
         case "4" :
             todo_number = input("enter the number of the task you want to delete: type cancel to return ")
             if todo_number != "cancel":
                 todos.pop(int(todo_number) - 1)
                 print("Done")
+
         case "5" :
             print("Exiting....")
-            file.close()
             exit()
