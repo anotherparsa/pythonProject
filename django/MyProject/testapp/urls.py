a39dc7ae71aa4b03642b2b13368eefa64938839d
anotherparsa <anotherparsa@gmail.com>
2025-01-16 07:05:28 +0330

    returning html files

--- django/MyProject/testapp/urls.py
+++ django/MyProject/testapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('home', views.index,  name='thisishome'),
+    path('home', views.home,  name='thisishome'),
     path('redirecting', views.redirecting),
     path('<test>', views.dynamicdata),
-
 ]
--- django/MyProject/testapp/views.py
+++ django/MyProject/testapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-# Create your views here.
 
-def index(request):
-    return HttpResponse("Hello World")
+
+
+def home(request):
+    return render(request, 'testapp/testapp.html')
 
 def dynamicdata(request, test):
     return HttpResponse(f'This is {test}')
